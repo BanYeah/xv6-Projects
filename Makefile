9276d1dc9c518ba525d3ee0dea8fcb2863c57db4
BanYeah <04smailing@gmail.com>
2025-03-21 23:25:05 +0900

    Fix: Testing meminfo

--- Makefile
+++ Makefile
@@ -140,6 +140,7 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_ps\
+	$U/_meminfo\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -95,5 +95,6 @@ meminfo(void)
   }
   release(&kmem.lock);
 
+  printf("available memory: %lu bytes\n", freemem * 4096);
   return freemem * 4096;
 }
